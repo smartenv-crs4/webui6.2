2c7f40dd7cd0627943d52bd4514368844b379199
michela <michelafancello@gmail.com>
2016-09-29 10:54:43 +0200

    modified custom

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,6 +8,7 @@ _access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtb2RlIjoibXMiLCJpc3MiOi
 var lng = localStorage.lng;
 
 jQuery(document).ready(function(){
+  $('#header_p').html(header_template);
   if (lng != undefined)
   {
     var l = jQuery(".languages a[data-lng='" + lng +"']");
@@ -16,43 +17,41 @@ jQuery(document).ready(function(){
       if(lng != jQuery(".languages .active a").first().attr("data-lng"))
       {
         var lngSel = jQuery(".languages .active").first();
-        lngSel.empty();      
+        lngSel.empty();
         lngSel.append(l[0].cloneNode(true));
         var c = document.createElement("i");
         c.className = "fa fa-check";
-        lngSel.find("a").first().append(c);      
+        lngSel.find("a").first().append(c);
         i18next.changeLanguage(lng, function(){});
       }
     }
   }
-  
+
   jQuery(".languages a").click(function(){
     if(jQuery(this).attr("data-lng"))
     {
       lng = jQuery(this).attr("data-lng");
       localStorage.lng = lng;
       var lngSel = jQuery(".languages .active").first();
-      lngSel.empty();      
+      lngSel.empty();
       lngSel.append(this.cloneNode(true));
       var c = document.createElement("i");
       c.className = "fa fa-check";
-      lngSel.find("a").first().append(c);  
+      lngSel.find("a").first().append(c);
       i18next.changeLanguage(lng, function(){});
     }
-    
+
   });
 
-  
+
 });
 
 
-i18next.use(i18nextXHRBackend);
+//i18next.use(i18nextXHRBackend);
 i18next.init({
   lng: lng, // evtl. use language-detector https://github.com/i18next/i18next-browser-languageDetector
   fallbackLng: "en",
-  backend: {
-    loadPath: 'assets/js/translations/{{lng}}/{{ns}}.json'
-  }
+  resources:  translation
 }, function (err, t) {
   jqueryI18next.init(i18next, jQuery,
   {
@@ -65,10 +64,10 @@ i18next.init({
     useOptionsAttr: false, // see optionsAttr
     parseDefaultValueFromContent: true // parses default values from content ele.val or ele.text
   });
-  
+
 });
 
-/*    
+/*
 jqueryI18next.init(i18next, jQuery,
 {
   tName: 't', // --> appends $.t = i18next.t
@@ -93,7 +92,7 @@ jQuery(document).ready(function(){
 /*******************************************
  ****************** UTILS ******************
  *******************************************/
-function isValidEmailAddress(emailAddress) 
+function isValidEmailAddress(emailAddress)
 {
   var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
   return pattern.test(emailAddress);
@@ -102,33 +101,33 @@ function isValidEmailAddress(emailAddress)
 
  /*******************************************
  *******************************************/
- 
+
 
 function signIn()
 {
   var email = jQuery("#signInEmail").val();
   var password = jQuery("#signInPassword").val();
-  
+
   var respBlock = jQuery("#signInResponse");
-  
+
   if(respBlock.is(":visible"))
   {
     respBlock.addClass("invisible");
   }
-  
-  
+
+
   if(!isValidEmailAddress(email))
-  {    
+  {
     respBlock.html(i18next.t("error.invalid_email"));
     respBlock.removeClass("invisible");
     return;
   }
-  
+
   var data = new Object();
   //data["user"] = new Object();
-  data["username"] = email; 
+  data["username"] = email;
   data["password"] = password;
-  
+
   jQuery.ajax({
     url: _userMsUrl + "users/signin",
     type: "POST",
@@ -136,7 +135,7 @@ function signIn()
     contentType: "application/json; charset=utf-8",
     dataType: "json",
     success: function(data, textStatus, xhr)
-    {            
+    {
       // success
       if(xhr.status == 201)
       {
@@ -145,18 +144,18 @@ function signIn()
       }
       // error
       else
-      {                
+      {
         respBlock.html(xhr.responseJSON.error_message);
         respBlock.removeClass("invisible");
-        return;                      
-      }    
-    },     
+        return;
+      }
+    },
     error: function(xhr, status)
-    {      
+    {
       console.log(xhr.status);
       switch(xhr.status)
       {
-        case 400: 
+        case 400:
           if(xhr.responseJSON.error == "invalid_token")
             respBlock.html(i18next.t("error.unauthorized"))
           else if(xhr.responseJSON.error == "BadRequest")
@@ -173,17 +172,17 @@ function signIn()
         default:
           respBlock.html(xhr.responseJSON.error_message);
       }
-      respBlock.removeClass("invisible");            
+      respBlock.removeClass("invisible");
       return;
-    },   
-    beforeSend: function(xhr, settings) 
-    { 
-      xhr.setRequestHeader('Authorization','Bearer ' + _access_token); 
-    }                   
+    },
+    beforeSend: function(xhr, settings)
+    {
+      xhr.setRequestHeader('Authorization','Bearer ' + _access_token);
+    }
   });
 }
-  
-  
+
+
 function signUp()
 {
   var email = jQuery("#signUpEmail").val();
@@ -191,44 +190,44 @@ function signUp()
   var password = jQuery("#signUpPassword").val();
   var password2 = jQuery("#signUpPassword2").val();
   var userType = jQuery("#signUpUserType").val();
-  
+
   var respBlock = jQuery("#signUpResponse");
-  
+
   if(respBlock.is(":visible"))
   {
     respBlock.addClass("invisible");
   }
-    
+
   if(!isValidEmailAddress(email))
   {
     respBlock.html(i18next.t("error.invalid_email"));
     respBlock.removeClass("invisible");
     return;
   }
-  
- 
+
+
   if(password !== password2 || password === "")
   {
     respBlock.html(i18next.t("error.password_differs"));
     respBlock.removeClass("invisible");
-    return;    
+    return;
   }
-  
-  
+
+
   if(userType !== "customer" && userType !== "supplier")
   {
     respBlock.html(i18next.t("error.unknown_user_type"));
     respBlock.removeClass("invisible");
-    return;    
+    return;
   }
-  
+
   var data = new Object();
   data["user"] = new Object();
-  data["user"]["email"] = email; 
-  data["user"]["name"] = name; 
+  data["user"]["email"] = email;
+  data["user"]["name"] = name;
   data["user"]["password"] = password;
   data["user"]["type"] = userType;
-  
+
   jQuery.ajax({
     url: _userMsUrl + "users/signup",
     type: "POST",
@@ -244,17 +243,17 @@ function signUp()
         sessionStorage.token = data["access_credentials"]["apiKey"]["token"];
       }
       else
-      {                
+      {
         respBlock.html(xhr.responseJSON.error_message);
         respBlock.removeClass("invisible");
-        return;                      
-      }           
-    },     
+        return;
+      }
+    },
     error: function(xhr, status)
-    {      
+    {
       switch(xhr.status)
       {
-        case 400: 
+        case 400:
           if(xhr.responseJSON.error == "invalid_token")
             respBlock.html(i18next.t("error.unauthorized"))
           else if(xhr.responseJSON.error == "BadRequest")
@@ -270,21 +269,21 @@ function signUp()
           break;
         case 500:
           respBlock.html(i18next.t("error.internal_server_error"));
-          break;        
+          break;
         default:
           respBlock.html(xhr.responseJSON.error_message);
       }
-      respBlock.removeClass("invisible");            
-      return;    
+      respBlock.removeClass("invisible");
+      return;
     },
-    beforeSend: function(xhr, settings) 
-    { 
-      xhr.setRequestHeader('Authorization','Bearer ' + _access_token); 
-    }                    
+    beforeSend: function(xhr, settings)
+    {
+      xhr.setRequestHeader('Authorization','Bearer ' + _access_token);
+    }
   });
-    
-    
+
+
 }
-  
-  
+
+
 
