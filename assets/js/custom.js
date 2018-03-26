ec517cb89e52661a7cfacf96b8cffe0b4a2b0737
davide.muroni <davide85@gmail.com>
2018-03-26 17:07:45 +0200

    fix

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -573,6 +573,8 @@ if(typeof Handlebars !== 'undefined')
   });
 
   Handlebars.registerHelper('json64', function(context) {
-        return btoa(JSON.stringify(context));
+        return btoa(encodeURIComponent(JSON.stringify(context)).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+          return String.fromCharCode(parseInt(p1, 16))
+        }));
   });
 }
