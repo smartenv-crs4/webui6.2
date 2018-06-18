a26cf1d41ed122ad58cc19b854339be8eb1f4b90
davide.muroni <davide85@gmail.com>
2018-06-18 12:41:50 +0200

    fix arrows in datepicker

--- assets/js/custom_rfq.js
+++ assets/js/custom_rfq.js
@@ -916,8 +916,8 @@ function openNewRfq(){
 
                 $('#expiredate').datepicker({
                     dateFormat: 'yy-mm-dd',
-                    prevText: '<i class="icon-chevron-left"></i>',
-                    nextText: '<i class="icon-chevron-right"></i>',
+                    prevText: '<i class="fa fa-angle-left"></i>',
+                    nextText: '<i class="fa fa-angle-right"></i>',
                     minDate: new Date(),
                     onSelect: function( selectedDate )
                     {
